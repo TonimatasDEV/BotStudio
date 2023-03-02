652df6240c239cd39972131791535a0c8dace5fb
TonimatasDEV <amatascubero@gmail.com>
2023-03-03 00:05:03 +0100

    Initial commit

---
+++ src/main/java/net/tonimatasdev/Main.java
@@ -0,0 +1,22 @@
+package net.tonimatasdev;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import net.tonimatasdev.listeners.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        JDABuilder builder = JDABuilder.createDefault("MTA3NDM0MTcwNjc4ODUwMzU2Mw.GdHm28.SOLSGzM0slM1RiswKHoTrPLatpe84TJ7a1dNtc");
+
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE, CacheFlag.ACTIVITY);
+        builder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
+        builder.setBulkDeleteSplittingEnabled(false);
+        builder.setActivity(Activity.playing("Starting"));
+
+        builder.addEventListeners(new Test());
+
+        builder.build();
+    }
+}
---
+++ src/main/java/net/tonimatasdev/listeners/Test.java
@@ -0,0 +1,17 @@
+package net.tonimatasdev.listeners;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Test extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        long time = System.currentTimeMillis();
+        event.reply("Pong!").setEphemeral(true)
+                .flatMap(v ->
+                        event.getHook().editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time) // then edit original
+                ).queue();
+    }
+
+
+}
