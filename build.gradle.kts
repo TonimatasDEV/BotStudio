4ddc739ce4a8092b00054bf043d30dfa2b11e604
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2023-10-09 18:57:02 +0200

    Update all libraries

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "dev.tonimatas"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-beta.15")
+}
