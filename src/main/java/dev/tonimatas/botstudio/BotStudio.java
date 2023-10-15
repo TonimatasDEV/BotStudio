dead27d7f4c8a05bd12e827fa64d5189f6225795
TonimatasDEV <amatascubero@gmail.com>
2023-10-15 13:53:03 +0200

    Use better log system and save logs

--- src/main/java/dev/tonimatas/botstudio/BotStudio.java
+++ src/main/java/dev/tonimatas/botstudio/BotStudio.java
@@ -7,15 +7,29 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.LogManager;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class BotStudio {
-    public static Logger logger = LogManager.getLogManager().getLogger("Server");
+    public static Logger logger = Logger.getLogger("Server");
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
+
+        new File("logs").mkdir();
+
+        try {
+            FileHandler fileHandler = new FileHandler("logs/latest.txt");
+            logger.addHandler(fileHandler);
+            SimpleFormatter simpleFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(simpleFormatter);
+        } catch (SecurityException | IOException e) {
+            logger.info("Exception:" + e.getMessage());
+        }
 
         File file = new File("key.txt");
 
