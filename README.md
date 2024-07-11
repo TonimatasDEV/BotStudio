0bce8b7be75b2bdf4ee0a44013e14ccf5b157e5e
TonimatasDEV <amatascubero@gmail.com>
2024-07-11 23:38:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BotStudio
+It is a discord bot for my community. You can join [here](https://discord.gg/5u3Ujgc8hy).
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+	"github.com/disgoorg/snowflake/v2"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+type Config struct {
+	Token string `json:"token"`
+}
+
+func main() {
+	config := readConfig("config.json")
+
+	client, err := disgo.New(config.Token,
+		bot.WithGatewayConfigOpts(gateway.WithIntents(gateway.IntentGuildMembers)),
+		bot.WithEventListenerFunc(onGuildMemberJoin),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = client.OpenGateway(context.TODO()); err != nil {
+		panic(err)
+	}
+
+	log.Println("Done!")
+
+	go func() {
+		stopChan := make(chan os.Signal, 1)
+		signal.Notify(stopChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-stopChan:
+			stop(client)
+		}
+	}()
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+	for {
+		rawCommand, err := inputReader.ReadString('\n')
+
+		command := strings.Replace(rawCommand, "\n", "", -1)
+
+		if err != nil {
+			log.Println("Exception on read the command:", err)
+		}
+
+		if command == "exit" || command == "stop" {
+			stop(client)
+		}
+
+		log.Println("Command:", command)
+	}
+}
+
+func readConfig(filename string) Config {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error unmarshalling config file: %v", err)
+	}
+
+	return config
+}
+
+func stop(client bot.Client) {
+	log.Println("BotStudio stopped successfully.")
+	client.Close(context.TODO())
+	os.Exit(0)
+}
+
+func onGuildMemberJoin(event *events.GuildMemberJoin) {
+	textChannelID, _ := snowflake.Parse("835907242544726057")
+	roleID, _ := snowflake.Parse("835907987641729064")
+
+	guild, _ := event.Client().Rest().GetGuild(event.GuildID, true)
+	channel, err := event.Client().Rest().GetChannel(textChannelID)
+
+	if err == nil {
+		welcomeMessage := fmt.Sprintf("%s Welcome to TonimatasDEV Studios! We already are: %d!", event.Member.User.Mention(), guild.ApproximateMemberCount)
+		_, err := event.Client().Rest().CreateMessage(channel.ID(), discord.NewMessageCreateBuilder().SetContent(welcomeMessage).Build())
+		if err != nil {
+			log.Printf("error sending welcome message: %v", err)
+		}
+	}
+
+	err = event.Client().Rest().AddMemberRole(event.GuildID, event.Member.User.ID, roleID)
+
+	if err != nil {
+		log.Printf("error adding role to member: %v", err)
+	}
+
+	log.Printf("%s joined. Count: %d", event.Member.User.Username, guild.ApproximateMemberCount)
+}
